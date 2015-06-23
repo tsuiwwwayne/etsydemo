2d240e24bb7d04e0dedc25932fa98fa4d90c6f7d
Wayne Tsui <tsuiwwwayne@hotmail.com>
2015-06-23 19:57:26 +0800

    Add Seller page

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   resources :listings
   resources :listings
   get 'pages/about'
-
   get 'pages/contact'
-
+  get 'seller' => "listings#seller"
   root 'listings#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
